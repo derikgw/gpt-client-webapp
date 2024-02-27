8b7191c7e1d921c61d9133f5e5997310392b2fde
derikgw <derikwilson6@gmail.com>
2024-02-27 05:39:07 -0500

    Fixed clear text display of secret key and added "**/secret_key.txt" to gitignore just in case.  This is a utility script used by the developer to generate the secret key that will be used in hashing.  This is not part of the application.  https://github.com/derikgw/gpt-client-webapp/security/code-scanning/7

--- scripts/gen_secret_key.py
+++ scripts/gen_secret_key.py
@@ -1,3 +1,22 @@
 import secrets
+import os
+
+# Generate a secret key
 secret_key = secrets.token_hex(16)
-print(secret_key)
+
+# Define the directory and filename where the secret key will be stored
+directory = os.path.expanduser('~/.gptclient')  # Expand the user's home directory
+filename = 'secret_key.txt'
+full_path = os.path.join(directory, filename)
+
+# Create the directory if it doesn't exist
+os.makedirs(directory, exist_ok=True)
+
+# Write the secret key to the file
+with open(full_path, 'w') as file:
+    file.write(secret_key)
+
+# Print the path, normalized and with forward slashes
+normalized_path = os.path.normpath(full_path).replace("\\", "/")
+print(f'Your secret key has been written to {normalized_path}')
+
